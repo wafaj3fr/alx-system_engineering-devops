0f6d7d247f47cb4f5f4af2d0cd20a134a523cc77
wafaj3fr <wafaagaafar@gmail.com>
2024-04-29 11:03:24 -0700

    2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,14 @@
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'start':
+  command => '/usr/sbin/service nginx start',
+}
