76aa95a08ba50b66cf016994266f210d0bb21602
wafaj3fr <wafaagaafar@gmail.com>
2024-05-14 09:48:35 -0700

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,14 +1,27 @@
-exec {'update':
-  command => '/usr/bin/apt-get update',
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
--> package {'nginx':
-  ensure => 'present',
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
 }
--> file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
--> exec {'start':
-  command => '/usr/sbin/service nginx start',
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
