4b5b0205d839ed8f53d6200679aabd40cb66b241
wafaj3fr <wafaagaafar@gmail.com>
2024-05-20 23:19:24 -0700

    03

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""eturns information about his/her TODO list progress"""
+"""For a given employee ID, returns information about
+their TODO list progress"""
+
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    uid = sys.argv[1]
+
+    userId = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(userId))
+
     name = user.json().get('name')
 
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    alltasks = 0
-    done = 0
+    totalTasks = 0
+    completed = 0
 
     for task in todos.json():
-        if task.get('uid') == int(uid):
-            alltasks += 1
-            if tasks.get('completed'):
-                done += 1
+        if task.get('userId') == int(userId):
+            totalTasks += 1
+            if task.get('completed'):
+                completed += 1
 
     print('Employee {} is done with tasks({}/{}):'
           .format(name, completed, totalTasks))
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-""" export data in the JSON format """
-
-import json
-import requests
-import sys
-
+"""Exports data in the JSON format"""
 
 if __name__ == "__main__":
-    jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
-    response = requests.get(jsonplaceholder)
-    employees = response.json()
-    print(employees)
 
-    data_dict = {}
+    import json
+    import requests
+    import sys
 
-    for employee in employees:
-        USER_ID = employee.get('id')
-        username = employee.get('username')
-        url = '{}/{}/todos'.format(jsonplaceholder, USER_ID)
-        response = requests.get(url)
-        tasks = response.json()
-        data_dict[USER_ID] = []
-        for task in tasks:
-            data_dict[USER_ID].append({
-                "username": username,
-                "task": task.get("title"),
-                "completed": task.get("completed"),
-            })
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
 
-    with open('todo_all_employees.json', 'w') as f:
-        json.dump(data_dict, f)
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
