4c06424b5a10138878a5b10ba0a0ea9ac83f5382
wafaj3fr <wafaagaafar@gmail.com>
2024-05-20 23:13:26 -0700

    01

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,43 +4,24 @@ import requests
 import sys
 
 
-def fetch_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    employee_url = f"{base_url}/users/{employee_id}"
-    response = requests.get(employee_url)
-    if response.status_code != 200:
-        print(f"Error: Unable to fetch employee details for ID {employee_id}")
-        return
-    employee_data = response.json()
-    employee_name = employee_data.get("name")
-
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-    response = requests.get(todos_url)
-    if response.status_code != 200:
-        print(f"Error: Unable to fetch TODO list for ID {employee_id}")
-        return
-    todos_data = response.json()
-
-    total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task.get("completed")]
-    number_of_done_tasks = len(done_tasks)
-
-    print(f"Employee {employee_name} \
-    is done with tasks({number_of_done_tasks}/{total_tasks}):")
-    for task in done_tasks:
-        print(f"\t {task.get('title')}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+    uid = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    name = user.json().get('name')
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Error: Employee ID must be an integer")
-        sys.exit(1)
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    alltasks = 0
+    done = 0
 
-    fetch_employee_todo_progress(employee_id)
+    for task in todos.json():
+        if task.get('uid') == int(uid):
+            alltasks += 1
+            if tasks.get('completed'):
+                done += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
+
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+          if task.get('userId') == int(userId) and task.get('completed')]))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""xports data in the CSV format"""
+"""exports data in the CSV format"""
+
 import requests
 from sys import argv
 
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    id = argv[1]
+if __name__ == "__main":
+    uid = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(uid))
+    name = user.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    user = requests.get(url + f"users/{id}").json()
-    todos = requests.get(url + f"todos", params={"userId": id}).json()
 
     file = f"{id}.csv"
     with open(file, "w") as f:
