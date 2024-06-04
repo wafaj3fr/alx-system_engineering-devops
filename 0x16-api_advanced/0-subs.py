e7956db751c949a36b14efd5f1db102f38b423e2
wafaj3fr <wafaagaafar@gmail.com>
2024-06-04 12:18:59 -0700

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,14 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    """Gets the number of subscribers of a subreddit"""
+    """
+    Gets the number of subscribers of a subreddit
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Prints:
+        The number of subscribers, or None if the request fails.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "CustomUserAgent/0.1"}
 
