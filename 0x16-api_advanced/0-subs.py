da84f59cc9db33ff16e8b38eebb0f662009bd9e0
wafaj3fr <wafaagaafar@gmail.com>
2024-06-04 12:05:50 -0700

    subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Number of subscribers"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """Gets the number of subscribers of a subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "CustomUserAgent/0.1"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
